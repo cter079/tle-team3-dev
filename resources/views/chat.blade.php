42722bcbb6793cf17e03abc1cbd83d4d5cca452e
Cter079 <cterlouw04@gmail.com>
2022-12-07 22:38:35 +0100

    opnieuw front-end

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -35,6 +35,9 @@
       </div>
 
       <div class="display">
+      <div class="screen3">
+      <div id="MyClockDisplay" class="clock" style="background-color: black; color:white; padding-left:5px;" onload="showTime()"></div>
+
         <div class="user-bar">
           <div class="back">
             <i class="zmdi zmdi-arrow-left"></i>
@@ -57,6 +60,7 @@
           </div>
         </div>
         <div class="form">
+          
 
 
           @foreach($chatContaints as $chat)
@@ -82,7 +86,7 @@
         </div>
         <div class="typing-field">
           <div class="input-data">
-            <select id="data" name="Select" required>
+            <select id="data" class="selectOption" name="Select" required>
               <option value="jaa">jaa</option>
               <option value="neeman">neeman</option>
 
@@ -91,9 +95,13 @@
             <input id="data3" type="hidden" value="{{$chat->name}}">
 
 
-            <button onclick="sendMessage()" id="send-btn">Send</button>
+            <button onclick="sendMessage()" class="sendButton" id="send-btn"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#219473" class="bi bi-send-fill" viewBox="0 0 16 16">
+  <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
+</svg></button>
+            
           </div>
         </div>
+</div>
 
       </div>
 
@@ -158,7 +166,7 @@
           setTimeout(function() {
           let replay = '<div class="bot-inbox inbox"><img class="profile-image" src="{{$chatDetails->image}}"><div class="msg-header"><p>' + body["replay"] + '</p></div></div>';
           document.querySelector(".form").insertAdjacentHTML("beforeend", replay);
-          // document.querySelector(".form").scrollTop(document.querySelector(".form")[0].scrollHeight);
+         document.querySelector(".form").scrollTop(document.querySelector(".form")[0].scrollHeight);
           document.querySelector(".status").textContent = "online";
 
 
@@ -176,6 +184,27 @@
       })
     }
   }
+  function showTime(){
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    
+    let time = h + ":" + m;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
 </script>
 
 </html>
