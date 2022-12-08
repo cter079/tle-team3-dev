09e266a790f37b5d63d203065fe73cd1e9a4add7
Cter079 <cterlouw04@gmail.com>
2022-12-08 13:22:28 +0100

    front-end

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -40,7 +40,10 @@
 
         <div class="user-bar">
           <div class="back">
-            <i class="zmdi zmdi-arrow-left"></i>
+            <i class="zmdi zmdi-arrow-left" onclick="window.history.go(-1); return false;"><svg 	style="cursor: pointer;"
+ xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+</svg></i>
           </div>
           <div class="avatar">
             <img src="{{$chatDetails->image}}" alt="Avatar" height="40px">
@@ -67,14 +70,20 @@
           @if($chat->direction == "received")
 
           <div class="bot-inbox inbox">
-            <img class="profile-image" src="{{$chatDetails->image}}">
+            <div class="arrow-left"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#219473" class="bi bi-triangle-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/>
+</svg></div>
             <div class="msg-header">
-              <p>{{$chat-> messages}}</p>
+
+              <p><span style="color:#915c00; font-weight: bold;">{{$chatDetails->name}}</span> 
+                <br>
+                {{$chat-> messages}}</p>
             </div>
           </div>
 
           @else
           <div class="user-inbox inbox">
+      
             <div class="msg-header">
               <p>{{$chat-> messages}}</p>
             </div>
@@ -95,8 +104,8 @@
             <input id="data3" type="hidden" value="{{$chat->name}}">
 
 
-            <button onclick="sendMessage()" class="sendButton" id="send-btn"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#219473" class="bi bi-send-fill" viewBox="0 0 16 16">
-  <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
+            <button onclick="sendMessage()" class="sendButton" id="send-btn"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-send" viewBox="0 0 16 16">
+  <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
 </svg></button>
             
           </div>
@@ -165,7 +174,6 @@
         setTimeout(function() {
           let replay = '<div class="bot-inbox inbox"><img class="profile-image" src="{{$chatDetails->image}}"><div class="msg-header"><p>' + body["replay"] + '</p></div></div>';
           document.querySelector(".form").insertAdjacentHTML("beforeend", replay);
-         document.querySelector(".form").scrollTop(document.querySelector(".form")[0].scrollHeight);
           document.querySelector(".status").textContent = "online";
 
 
