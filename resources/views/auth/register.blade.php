a98a3741248f8a895a9af5dfd8f6a8fdb1925a4c
Cter079 <cterlouw04@gmail.com>
2022-12-13 16:31:05 +0100

    css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,107 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Inloggen</title>
+
+    <link rel="stylesheet" type="text/css" href="authenticate.css" />
+
+
+</head>
+
 @extends('layouts.app')
 
 @section('content')
+<div class="wrapper">
+    <div class="device">
+      <div class="screen"></div>
+      <div class="btn-volume btn-volume-up"></div>
+      <div class="btn-volume btn-volume-down"></div>
+      <div class="btn-power">
+        <div class="btn-power-act"></div>
+      </div>
+
+      <div class="header">
+        <div class="detector"></div>
+        <div class="camera"></div>
+      </div>
+
+      <div class="display">
+      <div id="MyClockDisplay" class="clock" style="background-color: black; color:white; padding-left:5px;" onload="showTime()"></div>
+      
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+	<div class="screen4">
+        
+		<div class="screen__content">
+        <form method="POST" class="login" action="{{ route('register') }}">
+                @csrf
+                <img class="icon" src="https://techcrunch.com/wp-content/uploads/2013/04/burner-logo.png" />
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+				<div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+                    <input type="text" id="email" name="email" class="login__input" placeholder="User name / Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+				</div>
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+				<div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+                    <input type="password" id="password" class="login__input" placeholder="Password" name ="password" required autocomplete="current-password">
+				</div>
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+                <div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+                    <input type="password" id="password-confirm" class="login__input" placeholder="Confirm Password" name="password_confirmation" required autocomplete="new-password">
+				</div>
+                    <button type="submit" class="button login__submit">
+                        {{ __('Register') }}
+                    </button>		
+			</form>
+		</div>
+		<div class="screen__background">
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	</div>
 </div>
+<div class="footer">
+        <div class="btn-burger"></div>
+        <div onclick="window.location.href=`{{route('home')}}`;" class="btn-home"></div>
+        <div class="btn-back" onclick="window.history.go(-1); return false;"></div>
+      </div>
+    </div>
+<script>function showTime(){
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    
+    let time = h + ":" + m;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();</script>
 @endsection
