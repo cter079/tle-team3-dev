340ebbe0da72a8b7971a28038ea70c879691d018
Cter079 <cterlouw04@gmail.com>
2022-12-15 14:19:42 +0100

    fix chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -171,7 +171,7 @@
 
         
         setTimeout(function() {
-          let replay = '<div class="bot-inbox inbox"><img class="profile-image" src="{{$chatDetails->image}}"><div class="msg-header"><p>' + body["replay"] + '</p></div></div>';
+          let replay = '<div class="bot-inbox inbox"><div class="arrow-left"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#219473" class="bi bi-triangle-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/></svg></div><div class="msg-header"><p><span style="color:#915c00; font-weight: bold;">{{$chatDetails->name}}</span><br>' + body["replay"] + '</p></div></div>';
           document.querySelector(".form").insertAdjacentHTML("beforeend", replay);
           document.querySelector(".status").textContent = "online";
 
