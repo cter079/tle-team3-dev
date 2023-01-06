50a37e1baff0ef28fbef4a99106d54205e587dcc
Cter079 <cterlouw04@gmail.com>
2023-01-07 00:19:25 +0100

    service

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -12,6 +12,8 @@
   <meta http-equiv="Cache-Control" content="no-store" />
 <meta http-equiv="Pragma" content="no-store" />
 <meta http-equiv="Expires" content="0" />
+<meta http-equiv="refresh" content="0" />
+
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 
   <audio id="mySound" src="<?= asset('audio/notification.mp3') ?>"></audio>
