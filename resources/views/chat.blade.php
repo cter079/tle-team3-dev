8df31005549b7b2860cfb3d764f7d4d44c58cb26
Cter079 <cterlouw04@gmail.com>
2023-01-07 00:06:55 +0100

    cache update

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -9,8 +9,8 @@
 
   <link rel="stylesheet" href="app.css">
   <link rel="stylesheet" type="text/css" href="{{ asset('app.css') }}">
-  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-<meta http-equiv="Pragma" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Cache-Control" content="no-store" />
+<meta http-equiv="Pragma" content="no-store" />
 <meta http-equiv="Expires" content="0" />
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 
