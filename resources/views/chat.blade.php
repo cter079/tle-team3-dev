58169bb3ae8c2bcf77c391417b1b3f8a06e3bcd7
Cter079 <cterlouw04@gmail.com>
2023-01-07 00:25:43 +0100

    pls

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -11,7 +11,6 @@
   <link rel="stylesheet" type="text/css" href="{{ asset('app.css') }}">
   <meta http-equiv="Cache-Control" content="no-store" />
 <meta http-equiv="Pragma" content="no-store" />
-<meta http-equiv="Expires" content="0" />
 
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 
